c98ecbc637b867802798a3cb45192b1ecd0178f2
Henry Sloan <henryksloan@gmail.com>
2020-06-05 19:01:14 +0000

    Add exception safety to comment posting

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -91,11 +91,27 @@ public class DataServlet extends HttpServlet {
     Query query = new Query("User")
         .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
     PreparedQuery results = datastore.prepare(query);
-    Entity entity = results.asSingleEntity();
-    if (entity == null) {
+    Entity entity;
+
+    // Search for the user(s) with the matching ID
+    PreparedQuery results = datastore.prepare(query);
+    try {
+        // There should be either zero or one results, so try to treat it as one user
+        entity = results.asSingleEntity();
+        if (entity == null) { // Redirect if this user does not have a username registered
+            // TODO: Redirect to explanatory error page
+            response.sendRedirect("/index.html");
+            return;
+        }
+    }
+    catch (PreparedQuery.TooManyResultsException e) {
+        // There are multiple users with this ID; end the registration
+        // This is practically impossible but theoretically dangerous 
+        // TODO: Redirect to explanatory error page
         response.sendRedirect("/index.html");
         return;
     }
+
     String nickname = (String) entity.getProperty("nickname");
 
     long timestamp = System.currentTimeMillis();
